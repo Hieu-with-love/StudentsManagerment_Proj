5ae0565ab78e3b9a0a3a40a88d1c466a83a9dfe5
Tran Trung Hieu <tthieu@gmail.com>
2024-01-23 20:46:45 +0700

    just note

--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,10 @@ namespace StudentsManagerment_Proj
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Load data at here
+        }
     }
 }
