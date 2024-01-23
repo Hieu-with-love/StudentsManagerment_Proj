31988bc8d0bee3584c11f72a9f3bc90a48fa6201
Tran Trung Hieu <tthieu@gmail.com>
2024-01-23 22:09:35 +0700

    create DBConnection help for add,remove,edit more easy

---
+++ DBConnection.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data;
+using System.Data.SqlClient;
+
+
+namespace StudentsManagerment_Proj
+{
+    public class DBConnection
+    {
+        SqlConnection conn = new SqlConnection(@"Data Source=(localdb)\mssqllocaldb;Initial Catalog=StudentsManagerment;Integrated Security=True");
+        SqlCommand cmd;
+        DataTable dt;
+
+        public DataTable LoadData(string sqlQuery)
+        {
+            try
+            {
+                if (conn.State == ConnectionState.Closed) conn.Open();
+                cmd = new SqlCommand(sqlQuery, conn);
+                dt = new DataTable();
+                dt.Load(cmd.ExecuteReader());
+                return dt;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Tải dữ liệu thất bại.\n"+ex.Message);
+                return null;
+            }
+            finally
+            {
+                if (conn.State == ConnectionState.Open) conn.Close();
+            }
+        }
+
+        public int ExcuteQuery(string sql, SqlParameter[] lstParam)
+        {
+            try
+            {
+                if (conn.State == ConnectionState.Closed) conn.Open();
+                cmd = new SqlCommand(sql, conn);
+                cmd.Parameters.AddRange(lstParam);
+                dt.Load(cmd.ExecuteReader());
+                return 1;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Tải dữ liệu thất bại.\n"+ex.Message);
+                return 0;
+            }
+            finally
+            {
+                if (conn.State == ConnectionState.Open) conn.Close();
+            }
+        }
+    }
+}
